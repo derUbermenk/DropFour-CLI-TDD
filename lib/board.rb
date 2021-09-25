dffaeccd173851d73d78c1c738a6f8103588b316
Chester Asuncion <asuncionmcv@gmail.com>
2021-09-25 21:12:32 +0800

    refactoring code in order to pass test

--- lib/board.rb
+++ lib/board.rb
@@ -40,7 +40,13 @@ class Board
   end
 
   def diagonal_match
-    diagonal1, diagonal2 = get_diagonal_elements(@last_drop[:row], @last_drop[col])
+    piece_pattern = create_pattern(@last_drop[:piece])
+    diagonal1 = get_diagonal_elements(@last_drop[:row], @last_drop[:col], 1)
+    diagonal2 = get_diagonal_elements(@last_drop[:row], @last_drop[:col], -1)
+
+    [diagonal1, diagonal2].inject(false) do |has_match, diagonal|
+      has_match || diagonal.join.match?(piece_pattern)
+    end
   end
 
   # checks if board is full
@@ -60,11 +66,56 @@ class Board
   end
 
   # returns all elements of cells in given row
-  def get_row_elements(row); end
+  def get_row_elements(row) 
+    @cells[row]
+  end
 
   # returns all elements of cells in given column
-  def get_column_elements(column); end
+  def get_column_elements(column)
+    @cells.each_with_object([]) do |row, queried_column|
+      queried_column << row[column]
+    end
+  end
 
-  # returns all elements of cells in given diagonal
-  def get_diagonal_elements(diagonal); end
+  # returns the elements in the diagonal passing through 
+  # cell index by row column combo
+  # @param row [Integer]
+  # @param column [Integer]
+  # @param slope can either be 1 -- pointing to origin or
+  # ... -1 pointing away from origin [0][0]
+  # @return [Array] an array of elements contained by queried diagonal
+  def get_diagonal_elements(row, column, slope)
+    get_points(column, row, slope).each_with_object([]) do |point, elements|
+      elements << @cells[point[1]][point[0]]
+    end
+  end
+
+  # given points x and y, find all points in a line
+  # with a given slope within some x and y bounds
+  # @param x_point [Integer]
+  # @param y_point [Integer]
+  # @param slope [Integer]
+  # @param x_bound [Integer]
+  # @param y_bound [Integer]
+  # @return [Array] an Array containing all [x,y] value pairs
+  def get_points(x_point, y_point, slope, x_bound = 6, y_bound = 5)
+    y_int = y_intercept(x_point, y_point, slope)
+    x_values = [*0..6]
+    possible_y = [*0..5]
+
+    x_values.each_with_object([]) do |x, points|
+      y = (slope*x) + y_int
+      points << [x, y] if possible_y.include?(y)
+    end
+  end
+
+  # derives the y intercept of a line containing
+  # x, y given that it has given slope
+  # @param x [Integer]
+  # @param y [Integer]
+  # @param slope [Integer]
+  # @return [Integer] the y intercept
+  def y_intercept(x_point, y_point, slope)
+    y_point - (slope * x_point)
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -163,11 +163,12 @@ describe Board do
 
       it 'returns false' do
         match_check = board.horizontal_match
-        expect(match_check).to be false 
+        expect(match_check).to be false
       end
     end
   end
 
+  # if horizontal match works then so does vertical match
   describe '#vertical_match' do
     context 'when there is no match' do
       it 'returns false' do
@@ -180,6 +181,7 @@ describe Board do
     end
   end
 
+  # if horizontal match works then so does vertical match
   describe 'diagonal_match' do
     context 'when there is no match' do
       it 'returns false' do
@@ -230,4 +232,140 @@ describe Board do
       end
     end
   end
+
+  context 'private methods' do
+    subject(:board) { described_class.new }
+    
+    describe '#get_row_elements' do
+      context 'when querying for row 5' do
+        let(:row5){ [*1..7] } 
+        before do
+          cells = board.instance_variable_get(:@cells)
+          cells[5] = row5
+
+          board.instance_variable_set(:@cells, cells)
+        end
+
+        it 'returns an array containing the elements of row 6' do
+          queried_row = board.send(:get_row_elements, 5)
+          expect(queried_row).to eql(row5)
+        end
+      end
+    end
+
+    describe '#get_column_elements' do
+      context 'when querying for column 6' do
+        let(:column6) { [*1..6] }
+        before do
+          cells = board.instance_variable_get(:@cells)
+          # create a new array with zip, each element in zip contains
+          #  ... a cell row and column value pair, calling each changes 
+          #  ... each 6th element of each row to some col_val from column6
+          # B )
+          cells.zip(column6).each { |row, col_val| row[6] = col_val }
+          board.instance_variable_set(:@cells, cells)
+        end
+
+        it 'returns an array containing the elements in column 6' do
+          queried_column = board.send(:get_column_elements, 6)
+          expect(queried_column).to eql(column6)
+        end
+      end
+    end
+
+    describe '#get_diagonal_elements' do
+      context 'when querying the diagonals for cell[2][3]' do
+        let(:diagonal1) { [*1..6] }
+        let(:diagonal2) { [*0..5] }
+
+        before do
+          # for ease diagonal values are equal to row positions
+          cells = board.instance_variable_get(:@cells)
+
+          cells.zip(diagonal1, diagonal2.reverse).each do |row, diagonal1_val, diagonal2_val|
+            diagonal1_position = diagonal1_val
+            diagonal2_position = diagonal2_val
+
+            row[diagonal1_position] = diagonal1_val
+            row[diagonal2_position] = diagonal2_val
+          end
+
+          cells.instance_variable_set(:@cells, cells)
+        end
+
+        it 'returns a size 2 array of arrays containing diagonal bisectors of cell[2][3]' do
+          #queried_diagonal_1 = board.send(:get_diagonal_elements, 2, 3, 1)
+          #queried_diagonal_2 = board.send(:get_diagonal_elements, 2, 3, -1)
+
+
+          #queried_diagonals = [queried_diagonal_1, queried_diagonal_2]
+          queried_diagonals = [1,-1].each_with_object([]) do |slope, diagonals|
+            diagonals << board.send(:get_diagonal_elements, 2, 3, slope)
+          end
+
+          expect(queried_diagonals).to eql([diagonal1, diagonal2])
+        end
+      end
+
+      context 'when querying the diagonals for cell[5][6]' do
+        let(:diagonal1) { [*1..6] }
+        before do
+          cells = board.instance_variable_get(:@cells)
+
+          cells.zip(diagonal1).each do |row, diagonal_val|
+            diagonal_pos = diagonal_val
+
+            row[diagonal_pos] = diagonal_val
+          end
+        end
+        it 'returns a size 1 array of arrays containing diagonal bisector of cell[5][6]' do
+          queried_diagonals = [1,-1].each_with_object([]) do |slope, diagonals|
+            diagonals << board.send(:get_diagonal_elements, 5, 6, slope)
+          end
+          expect(queried_diagonals).to eql([diagonal1])
+        end
+      end
+    end
+
+    describe '#get_points' do
+      it 'returns the points withing the bounds of lines x1 = 0, y1 = 0, x2 = 5, y2 = 6' do; end
+      
+      context 'when querying the points of a positive sloped line' do
+        context 'when querying for the points in line passing through (1, 3)' do
+          it 'returns [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5]]' do
+            expected_points = [*0..4].zip([*1..5])
+            queried_points = board.send(:get_points, 2, 3, 1)
+            expect(queried_points).to eql(expected_points)
+          end
+        end
+
+        context 'when querying for the points in line passing through cell[0][2]' do
+          it 'returns [[2, 0], [3, 1], [4, 2], [5, 3], [6, 4]]' do
+            expected_points = [*2..6].zip [*0..4]
+            queried_points = board.send(:get_points, 2, 0, 1)
+            expect(queried_points).to eql(expected_points)
+          end
+        end
+      end
+
+      context 'when querying for the points in a negative sloped line' do
+        context 'when querying for the points in line passing through (3, 5)' do
+          it 'returns [3, 5], [4, 4], [5, 3], [6, 2]' do
+            expected_points = [*3..6].zip [*2..5].reverse
+            queried_points = board.send(:get_points, 3, 5, -1)
+            expect(queried_points).to eql(expected_points)
+          end
+        end
+
+        context 'when querying for the points in line passing through (1, 2)' do
+          it 'returns [0, 3], [1, 2], [2, 1], [3, 0]' do
+            expected_points = [*0..3].zip [*0..3].reverse
+            queried_points = board.send(:get_points, 1, 2, -1)
+            expect(queried_points).to eql(expected_points)
+          end
+        end
+      end
+    end
+  end
 end
+
